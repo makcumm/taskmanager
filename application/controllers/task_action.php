28ae10d2f3c6ea5f1766fcce2a145cdae9f87e99
mm <mm@inleadmedia.com>
2013-04-01 13:05:33 +0300

    Move task action methods to appropriate classes

--- application/controllers/task_action.php
+++ application/controllers/task_action.php
@@ -7,44 +7,44 @@ class Task_action extends CI_Controller {
 		parent::__construct();
 	}
 
-	public function add_task()
+	function add_task()
 	{
 		if ( $this->input->post( 'ajax' ) )
 		{
 			$validation_rules = array(
 				array(
 					'field' => 'structure',
-					'label' => 'Структура',
+					'label' => 'Structure',
 					'rules' => 'required|numeric'
 				),
 				array(
 					'field' => 'division',
-					'label' => 'Подразделение',
+					'label' => 'Division',
 					'rules' => 'required|numeric'
 				),
 				array(
 					'field' => 'importance',
-					'label' => 'Приоритет',
+					'label' => 'Priority',
 					'rules' => 'trim'
 				),
 				array(
 					'field' => 'worker',
-					'label' => 'Ответственный',
+					'label' => 'Responsible',
 					'rules' => 'required|numeric'
 				),
 				array(
 					'field' => 'task_name',
-					'label' => 'Название задачи',
+					'label' => 'Task Name',
 					'rules' => 'required|trim|xss_clean'
 				),
 				array(
 					'field' => 'task_description',
-					'label' => 'Описание задачи',
+					'label' => 'Description',
 					'rules' => ''
 				),
 				array(
 					'field' => 'task_deadline',
-					'label' => 'Дата окончания',
+					'label' => 'End date',
 					'rules' => ''
 				)
 			);
@@ -55,11 +55,11 @@ class Task_action extends CI_Controller {
 
 			if ( $this->form_validation->run() == TRUE )
 			{
-				$this->modal_model->add_task();
+				$this->main_m->add_task();
 
 				$message = array(
 					'status' => "ok",
-					'message' => "Задача добавлена"
+					'message' => "Congratulation! New task added."
 				);
 			}
 			else
@@ -75,21 +75,17 @@ class Task_action extends CI_Controller {
 					'task_deadline' => form_error( 'task_deadline', '<p class="alert alert-error">', '</p>' )
 				);
 			}
-
-			$data['modal_data'] = array(
-				'message' => $message
-			);
-			// echo $this->load->view( 'pages/helpers/modal_view' );
+			echo json_encode( $message );
 		}
 	}
 
-	public function edit_task()
+	function task_remove()
 	{
 		if ( $this->input->post( 'ajax' ) )
 		{
 			$task_id = $this->input->post( 'task_id' );
 
-			if ( $this->modal_model->task_remove( $task_id ) )
+			if ( $this->main_m->task_remove( $task_id ) )
 			{
 				echo json_encode( array( 'status' => 'ok' ) );
 			}
@@ -97,6 +93,97 @@ class Task_action extends CI_Controller {
 			{
 				echo json_encode( array( 'status' => 'error' ) );
 			}
+		}
+	}
+
+	function task_made()
+	{
+		if ( $this->input->post( 'ajax' ) )
+		{
+			$task_id = $this->input->post( 'task_id' );
+			$status = $this->input->post( 'status' );
+
+			if ( $this->main_m->task_made( $task_id, $status ) )
+			{
+				echo json_encode( array( 'status' => 'ok' ) );
+			}
+			else
+			{
+				echo json_encode( array( 'status' => 'error' ) );
+			}
+		}
+	}
+
+	function task_edit()
+	{
+//		$this->output->enable_profiler(TRUE);
+		if ( $this->input->post( 'ajax' ) )
+		{
+			$valid_rules = array(
+				array(
+					'field' => 'structure',
+					'label' => 'Structure',
+					'rules' => 'required|numeric'
+				),
+				array(
+					'field' => 'division',
+					'label' => 'Division',
+					'rules' => 'required|numeric'
+				),
+				array(
+					'field' => 'importance',
+					'label' => 'Priority',
+					'rules' => 'trim'
+				),
+				array(
+					'field' => 'worker',
+					'label' => 'Responsible',
+					'rules' => 'required|numeric'
+				),
+				array(
+					'field' => 'task_name',
+					'label' => 'Task Name',
+					'rules' => 'required|trim|xss_clean'
+				),
+				array(
+					'field' => 'task_description',
+					'label' => 'Description',
+					'rules' => ''
+				),
+				array(
+					'field' => 'task_deadline',
+					'label' => 'End date',
+					'rules' => ''
+				)
+			);
+
+			$this->form_validation->set_rules( $valid_rules );
+
+			$message = array();
+
+			if ( $this->form_validation->run() == TRUE )
+			{
+				$this->main_m->task_edit();
+
+				$message = array(
+					'status' => "ok",
+					'message' => "Task updated."
+				);
+			}
+			else
+			{
+				$message = array(
+					'status' => "error",
+					'structure' => form_error( 'structure', '<p class="alert alert-error">', '</p>' ),
+					'division' => form_error( 'division', '<p class="alert alert-error">', '</p>' ),
+					'importance' => form_error( 'importance', '<p class="alert alert-error">', '</p>' ),
+					'worker' => form_error( 'worker', '<p class="alert alert-error">', '</p>' ),
+					'task_name' => form_error( 'task_name', '<p class="alert alert-error">', '</p>' ),
+					'task_description' => form_error( 'task_description', '<p class="alert alert-error">', '</p>' ),
+					'task_deadline' => form_error( 'task_deadline', '<p class="alert alert-error">', '</p>' )
+				);
+			}
+			echo json_encode( $message );
 		}
 	}
 
