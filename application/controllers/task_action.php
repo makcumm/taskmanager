e6ae7d89759d7626f925cfbead95ef7746c099dd
mm <mm@inleadmedia.com>
2013-04-08 13:13:43 +0300

    Move validation code from controller to model class

--- application/controllers/task_action.php
+++ application/controllers/task_action.php
@@ -5,77 +5,14 @@ class Task_action extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->model('task_action_model');
 	}
 
 	function add_task()
 	{
-//		var_dump($this->input->post()); exit();
 		if ( $this->input->post( 'ajax' ) )
 		{
-			$validation_rules = array(
-				array(
-					'field' => 'structure',
-					'label' => 'Structure',
-					'rules' => 'required|numeric'
-				),
-				array(
-					'field' => 'division',
-					'label' => 'Division',
-					'rules' => 'required|numeric'
-				),
-				array(
-					'field' => 'importance',
-					'label' => 'Priority',
-					'rules' => 'trim'
-				),
-				array(
-					'field' => 'worker',
-					'label' => 'Responsible',
-					'rules' => 'required|numeric'
-				),
-				array(
-					'field' => 'task_name',
-					'label' => 'Task Name',
-					'rules' => 'required|trim|xss_clean'
-				),
-				array(
-					'field' => 'task_description',
-					'label' => 'Description',
-					'rules' => ''
-				),
-				array(
-					'field' => 'task_deadline',
-					'label' => 'End date',
-					'rules' => ''
-				)
-			);
-
-			$this->form_validation->set_rules( $validation_rules );
-
-			$message = array();
-
-			if ( $this->form_validation->run() == TRUE )
-			{
-				$this->main_m->add_task();
-
-				$message = array(
-					'status' => "ok",
-					'message' => "Congratulation! New task added."
-				);
-			}
-			else
-			{
-				$message = array(
-					'status' => "error",
-					'structure' => form_error( 'structure', '<p class="alert alert-error">', '</p>' ),
-					'division' => form_error( 'division', '<p class="alert alert-error">', '</p>' ),
-					'importance' => form_error( 'importance', '<p class="alert alert-error">', '</p>' ),
-					'worker' => form_error( 'worker', '<p class="alert alert-error">', '</p>' ),
-					'task_name' => form_error( 'task_name', '<p class="alert alert-error">', '</p>' ),
-					'task_description' => form_error( 'task_description', '<p class="alert alert-error">', '</p>' ),
-					'task_deadline' => form_error( 'task_deadline', '<p class="alert alert-error">', '</p>' )
-				);
-			}
+			$message = $this->task_action_model->task_add_validate();
 			echo json_encode( $message );
 		}
 	}
